296fb1bbfae40e2735701f0baa5cab26fd184eef
Anelie Decomotan <anelie.decomotan@teloworksinc.com>
2018-02-23 22:42:26 +0800

    List users

--- api/admin.php
+++ api/admin.php
@@ -1,6 +1,14 @@
 <?php
-$users = array();
-$users[] = ['username' => 'hello', 'email' => 'hello@world.com'];
-$users[] = ['username' => 'world', 'email' => 'wolrd@hello.com'];
+$config = parse_ini_file('config.ini');
+try {
+	$pdo = new PDO('mysql:host=' . $config['db_server'] . ';dbname=' . $config['db_name'], $config['db_user'], $config['db_password']);
+	$statement=$pdo->prepare("SELECT * FROM users");
+	$statement->execute();
+	$results=$statement->fetchAll(PDO::FETCH_ASSOC);
+    print_r(json_encode($results));
+} catch (Exception $e) {
+    header('Content-Type: application/json');
+    print_r(json_encode("An error was encountered while saving the user record: " . $e->getMessage()));
+}
 
-print_r(json_encode($users));
+
